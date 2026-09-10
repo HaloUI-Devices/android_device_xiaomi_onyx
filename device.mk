2da424c30f4087aa687a116b3bfbc40622c1ffcb
AdarshGrewal <adarshgrewal@gmail.com>
2026-09-10 09:52:15 +0530

    onyx: Setup lights aidl
    Change-Id: I39c2764248356e5d00e356cb378c4ea2d41d71e3

--- device.mk
+++ device.mk
@@ -287,6 +287,10 @@ PRODUCT_COPY_FILES += \
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.onyx
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
